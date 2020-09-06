854b6c1212f523af04362616296f20aab0cbf273
ranyk <1390851149@qq.com>
2020-09-06 19:37:05 +0800

    初始化项目

---
+++ ssv-admin/src/test/java/com/ranyk/ssv/SsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranyk.ssv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
