abe67c8fc46bc7b6f1293b27a32fb50968668416
ranyk <1390851149@qq.com>
2020-12-02 14:04:06 +0800

    增加 Swagger 和 Spring Security 的集成

--- ssv-common/src/main/java/com/ranyk/ssv/common/constent/BaseEnumConstantInterfaceVO.java
+++ ssv-common/src/main/java/com/ranyk/ssv/common/constent/BaseEnumConstantInterfaceVO.java
@@ -1,12 +1,27 @@
 package com.ranyk.ssv.common.constent;
 
 /**
- * ClassName:BaseEnumConstentInterfaceVO
+ * ClassName:BaseEnumConstantInterfaceVO
  * Description:获取常量值的通用接口
  *
  * @author ranyi
  * @date 2020-11-10 10:36
  * Version: V1.0
  */
-public interface BaseEnumConstantInterfaceVO {
+public interface BaseEnumConstantInterfaceVO<T> {
+
+    /**
+     * 获取指定的常量值
+     * @return 返回给定类型的常量值
+     */
+    T getValue();
+
+    /**
+     * 设置指定的常量值
+     *
+     * @param value 需设置的值
+     */
+    void setValue(T value);
+
+
 }
